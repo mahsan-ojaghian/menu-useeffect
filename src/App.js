3eb756079d8cbe97ed17e6147c978e010a0df7df
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-19 16:58:26 +0430

    images added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import "./styles.css";
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+import './styles.css';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
-export default function App() {
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
+
+export default App;
